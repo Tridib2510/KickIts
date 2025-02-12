6f5491e045fa0eba523ae8161118c908599d122e
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2025-02-13 00:35:25 +0530

    menu

--- FrontEnd/AllEvents.js
+++ FrontEnd/AllEvents.js
@@ -1,12 +1,23 @@
 let newData
 
-fetch('http://127.0.0.1:8000/KickIt/home')
+
+let Events=document.createElement('div')
+
+function helper(text){
+Events=document.createElement('div')
+fetch(`https://kickits-1.onrender.com/KickIt/home${text}`)
+
+
 .then(res=>res.json())
 .then(data=>{
     
+if(data.status && data.status=='fail'){
+   document.body.events=false
+}
    
    for(i=0;i<data.data.length;i++){
     const div=document.createElement('div')
+    
     const eventName=document.createElement('h2')
       eventName.innerHTML=data.data[i].activity
       eventName.className="event-name"
@@ -22,10 +33,34 @@ fetch('http://127.0.0.1:8000/KickIt/home')
      const eventTime=document.createElement('p')
      eventTime.innerHTML=`Time : ${data.data[i].time}`
      div.appendChild(eventTime)
-    document.body.appendChild(div)
+     div.addEventListener('click', () => {
+     console.log('hi')
+   })
+   Events.appendChild(div)
+   document.body.events=true
+    document.body.appendChild(Events)
    }
 
 })
 .catch(err=>console.log(err))
+}
+
+helper('')
+
+const button=document.getElementById('search-button').addEventListener('click',()=>{
+  let text=''
+   const b=document.getElementById('search-bar').value
+   b.placeholder='hello'
+   if(b!='') text=`?activity=${b}`
+   if(document.body.events===true)
+      document.body.removeChild(Events)
+   
+   
+   console.log(text)
+   helper(text)
+
+})
+
+
 
 
