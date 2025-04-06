d2fb3106ac7538693731ce24708c0385dbd4a9b7
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2025-04-06 12:15:15 +0530

    reset and forgot password implementation

--- .vercel/output/static/resetPassword.js
+++ .vercel/output/static/resetPassword.js
@@ -5,6 +5,7 @@ const queryString=window.location.search;
 const urlParams=new URLSearchParams(queryString);
 const token=urlParams.get('token')
 document.getElementById('resetPasswordForm').addEventListener('submit', async (e) => {
+    event.preventDefault()
     const passwordReset = document.getElementById('new-password').value
     const passwordResetConfirm = document.getElementById('confirm-password').value
     fetch(`${url}/KickIt/resetPassword/${token}`, {
@@ -21,8 +22,18 @@ document.getElementById('resetPasswordForm').addEventListener('submit', async (e
     
 }).then(res => res.json())
 .then(data=>{
+    if (data.status==='fail'||data.status==='Error') {
+        console.log(data);
+        throw new Error(data.message);
+    }
+    else
    window.location.href = '/login.html'
 })
-.catch(err => console.log(err))
+.catch(err =>{
+        const errorContainer=document.getElementById('error-container')
+        
+    console.log(err.message)
+        errorContainer.textContent = `Error: ${err.message}`; // Display the error message
+})
 })
 
