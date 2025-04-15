b85d91a20ab68b97b196cf2f31003eaf53c86230
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2025-04-15 20:38:59 +0530

    push

--- FrontEnd/AllEvents.js
+++ FrontEnd/AllEvents.js
@@ -228,6 +228,7 @@ const button = document.getElementById('search-button').addEventListener('click'
     const text2=`?venue=${searchBar.value}`
     const text3=`?eventName=${searchBar.value}`
     if (document.body.events === true) {
+        if(leftDiv.contains(Events))
                 leftDiv.removeChild(Events);
            }
     helper(text1,'&page=1&limit=5')
