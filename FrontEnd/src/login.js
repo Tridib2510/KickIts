025ca9a50fb47b6a92c4d393beb93be7cc7f8009
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2025-06-17 14:36:19 +0530

    push guru

--- FrontEnd/src/login.js
+++ FrontEnd/src/login.js
@@ -1,4 +1,78 @@
 import url from "./ApiUrl.js";
+const client = new Appwrite.Client()
+
+ .setEndpoint('https://fra.cloud.appwrite.io/v1')
+ .setProject('68507be10009887f1337')
+
+  const account = new Appwrite.Account(client);
+
+
+async function handleLogin(){
+    account.createOAuth2Session(
+        'google',
+        `${url}/FrontEnd/src/index.html`,
+        `${url}/FrontEnd/src/index.html`
+    )
+   account.get()
+  .then(user => {
+
+    account.get()
+  .then(user => {
+
+   const data={
+    "email":`${user.email}`,
+     "password":`${user.$id}`,
+     
+     
+   }
+     const options = {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            data:data
+        })
+    };
+    fetch(`${url}/KickIt/login`,options).
+    then(res=>{
+   console.log(res)
+        
+        return res.json(); // Parse the JSON response
+
+    })
+    .then(data=>{
+        console.log(data)
+        if (data.status==='fail') {
+            console.log(data.message)
+            throw new Error(data.message);
+        }
+    window.location.href="index.html"
+    })
+    .catch(err=>{
+      
+        console.log(err.message)
+        errorContainer.textContent = `Error: ${err.message}`; // Display the error message
+    })
+     console.log(data)
+   
+  })
+  .catch(error => {
+    console.error('Failed to get user account:', error);
+  });
+
+   
+  })
+  .catch(error => {
+    console.error('Failed to get user account:', error);
+  });
+   
+}
+
+document.getElementById('google-btn').addEventListener('click',()=>{
+    handleLogin()
+})
 const errorContainer=document.getElementById('error-container')
 const form=document.getElementById('login')
 console.log(url)
--- FrontEnd/src/signUp.js
+++ FrontEnd/src/signUp.js
@@ -1,12 +1,86 @@
 import url from "./ApiUrl.js";
+
+const client = new Appwrite.Client()
+ .setEndpoint('https://fra.cloud.appwrite.io/v1')
+ .setProject('68507be10009887f1337')
+
+  const account = new Appwrite.Account(client);
+
+
+async function handleSignUp(){
+    account.createOAuth2Session(
+        'google',
+       `${url}/FrontEnd/src/index.html`,
+        `${url}/FrontEnd/src/index.html`
+    )
+   account.get()
+  .then(user => {
+
+    account.get()
+  .then(user => {
+
+   const data={
+     "username":`${user.name}`,
+     "password":`${user.$id}`,
+     "email":`${user.email}`,
+     "confirm_password":`${user.$id}`
+   }
+
+     const options = {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            data:data
+        })
+    };
+    fetch(`${url}/KickIt/signUp`,options).
+    then(res=>{
+   console.log(res)
+        
+        return res.json(); // Parse the JSON response
+
+    })
+    .then(data=>{
+        
+        if (data.status==='fail'||data.status==='Error') {
+            console.log(data.message)
+            throw new Error(data.message);
+        }
+    // window.location.href="AllEvents.html"
+    })
+    .catch(err=>{
+       // console.log('hello')
+        console.log(err.message)
+        errorContainer.textContent = `Error: ${err.message}`; // Display the error message
+    })
+  })
+  .catch(error => {
+    console.error('Failed to get user account:', error);
+  });
+
+  
+  })
+   
+}
+
+document.getElementById('google-btn').addEventListener('click',()=>{
+    handleSignUp()
+})
+
+
 const errorContainer=document.getElementById('error-container')
 const form=document.getElementById('signup')
 console.log(url)
 form.addEventListener('submit',(event)=>{
+
     event.preventDefault()
     console.log('Signup form submitted!');
     const formData = new FormData(form);
     const data=Object.fromEntries(formData.entries())
+    console.log(data)
     const options = {
         method: 'POST',
         credentials: 'include',
@@ -25,11 +99,12 @@ form.addEventListener('submit',(event)=>{
 
     })
     .then(data=>{
+        
         if (data.status==='fail'||data.status==='Error') {
             console.log(data.message)
             throw new Error(data.message);
         }
-     window.location.href="AllEvents.html"
+     //window.location.href="AllEvents.html"
     })
     .catch(err=>{
        // console.log('hello')
